e79cd70cda581050ac7922a440d1c7b719149d7d
dmtrbb11 <dmitriy.bobrov.2001@mail.ru>
2024-03-29 12:29:06 +0400

    3_lesson

---
+++ config/build/buildWebpackConfiguration.ts
@@ -0,0 +1,29 @@
+import { BuildOptions } from "./types/config";
+import webpack, { ids } from "webpack";
+import { buildLoaders } from "./buildLoaders";
+import { buildResolvers } from "./buildResolvers";
+import { buildPlugins } from "./buildPlugins";
+import { buildServer } from "./buildServer";
+
+export function buildWebpackConfig(
+  options: BuildOptions
+): webpack.Configuration {
+  const { paths, mode, isDev } = options;
+
+  return {
+    mode: mode,
+    entry: paths.entry,
+    devtool: isDev ? "inline-source-map" : undefined,
+    module: {
+      rules: buildLoaders(),
+    },
+    resolve: buildResolvers(),
+    output: {
+      filename: "[name].[contenthash].js",
+      path: paths.build,
+      clean: true,
+    },
+    plugins: buildPlugins(options),
+    devServer: isDev ? buildServer(options) : undefined,
+  };
+}
---
+++ config/build/types/config.ts
@@ -0,0 +1,19 @@
+export type BuildMode = "production" | "development"; // тип для строки которая выставляеет какой mode у webpack
+
+export interface BuildPaths {
+  entry: string; // путь до энтри пойинт
+  build: string; // путь до папки build с файлами которые минифицированы
+  html: string; // путь до index.html файла
+}
+
+export interface BuildEnv {
+  mode: BuildMode;
+  port: number;
+}
+
+export interface BuildOptions {
+  mode: BuildMode;
+  paths: BuildPaths;
+  isDev: boolean;
+  port: number;
+}
