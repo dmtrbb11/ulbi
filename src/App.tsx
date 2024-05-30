9604731aab8f28fffcdd007e1b0644e29b194718
dmtrbb11 <dmitriy.bobrov.2001@mail.ru>
2024-05-30 15:44:02 +0400

    8_lesson

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
-import Counter from "./components/Counter";
-import "./index.scss";
+import React, { useContext, useState } from "react";
+import "./styles/index.scss";
+import { Route, Routes } from "react-router";
+import { Link } from "react-router-dom";
+import { MainPageLazy } from "./pages/MainPage/MainPage.lazy";
+import { AboutPageLazy } from "./pages/AboutPage/AboutPage.lazy";
+import { Suspense } from "react";
+import { useTheme } from "./theme/useTheme";
+
+enum Theme {
+  LIGHT = "light",
+  DARK = "dark",
+}
 
 const App = () => {
+  const { theme, changeTheme } = useTheme();
   return (
-    <div className="app">
-      gogogogogogoo
-      <Counter />
+    <div className={`app ${theme} `}>
+      <button onClick={changeTheme}>Theme</button>
+      <Link to={"/"}>Главная</Link>
+      <br />
+      <Link to={"/about"}>О сайте</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={"/"} element={<MainPageLazy />} />
+          <Route path={"/about"} element={<AboutPageLazy />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,14 @@
 import { render } from "react-dom";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import ThemeProvider from "./theme/ThemeProvider";
 
-render(<App />, document.getElementById("root"));
+render(
+  <ThemeProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ThemeProvider>,
+
+  document.getElementById("root")
+);
